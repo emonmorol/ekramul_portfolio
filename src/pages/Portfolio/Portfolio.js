62077a5120f7a3021f45cd6ec040cbe98178b66d
emonmorol <ekramulhasane69@gmail.com>
2022-06-03 01:36:11 +0600

    portfolio projects

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,44 +1,99 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
 
 const Portfolio = () => {
+  const projects = [
+    {
+      id: 1,
+      title: "Mern Development",
+      description:
+        "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris",
+      icon: (
+        <>
+          <i class="fa-solid fa-layer-group"></i>
+        </>
+      ),
+      image:
+        "https://www.flexgigzz.com/wp-content/uploads/2021/04/bexer-bootstrap-business-template.jpg",
+    },
+    {
+      id: 2,
+      title: "Frontend Development",
+      description:
+        "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris",
+      icon: (
+        <>
+          <i class="fa-solid fa-code"></i>
+        </>
+      ),
+      image:
+        "https://www.flexgigzz.com/wp-content/uploads/2021/04/bexer-bootstrap-business-template.jpg",
+    },
+    {
+      id: 3,
+      title: "Backend Development",
+      description:
+        "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris",
+      icon: (
+        <>
+          <i class="fa-solid fa-server"></i>
+        </>
+      ),
+      image:
+        "https://www.flexgigzz.com/wp-content/uploads/2021/04/bexer-bootstrap-business-template.jpg",
+    },
+    {
+      id: 4,
+      title: "React Development",
+      description:
+        "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris",
+      icon: (
+        <>
+          <i class="fa-brands fa-react"></i>
+        </>
+      ),
+      image:
+        "https://www.flexgigzz.com/wp-content/uploads/2021/04/bexer-bootstrap-business-template.jpg",
+    },
+    {
+      id: 5,
+      title: "Javascript Development",
+      description:
+        "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris",
+      icon: (
+        <>
+          <i class="fa-brands fa-js-square"></i>
+        </>
+      ),
+      image:
+        "https://www.flexgigzz.com/wp-content/uploads/2021/04/bexer-bootstrap-business-template.jpg",
+    },
+    {
+      id: 6,
+      title: "Javascript Development",
+      description:
+        "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris",
+      icon: (
+        <>
+          <i class="fa-brands fa-js-square"></i>
+        </>
+      ),
+      image:
+        "https://www.flexgigzz.com/wp-content/uploads/2021/04/bexer-bootstrap-business-template.jpg",
+    },
+  ];
+
   return (
-    <div id="portfolio" className="text-white">
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
-      <h2>portfolio</h2>
+    <div id="portfolio" className="text-white max-w-7xl mx-auto">
+      <p className="text-base text-secondary uppercase">
+        VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK
+      </p>
+      <p className="text-accent font-bold text-6xl mb-10">Portfolio</p>
+      <div className="grid grid-cols-3 gap-10">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/pages/Portfolio/ProjectCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Portfolio.css";
+
+const ProjectCard = ({ project }) => {
+  const { title, image } = project;
+  return (
+    <div className="portfolio-card text-accent hover:text-white">
+      <div className="text-left">
+        <div className="rounded-xl overflow-hidden">
+          <img src={image} alt="" />
+        </div>
+        <div>
+          <h2 className="font-semibold text-sm my-4 text-secondary">{title}</h2>
+          <h2 className="font-semibold text-xl my-4 text-accent hover:text-secondary">
+            {title}
+          </h2>
+        </div>
+        <div className="button-content flex justify-between mt-5">
+          <button className="project-button">
+            <i class="text-sm text-secondary mr-2 fa-solid fa-circle-dot"></i>
+            View Live
+          </button>
+          <button className="project-button">
+            <i class="text-sm text-secondary mr-2 fa-brands fa-github"></i>
+            Client Code
+          </button>
+          <button className="project-button">
+            {" "}
+            <i class="text-sm text-secondary mr-2 fa-brands fa-github"></i>
+            Server Code
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
