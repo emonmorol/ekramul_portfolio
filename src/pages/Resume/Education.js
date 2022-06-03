9437a61c8fb2b7ada1af42b02a6455898432fa23
emonmorol <ekramulhasane69@gmail.com>
2022-06-03 17:57:01 +0600

    education timeline created

--- src/pages/Resume/Education.js
+++ src/pages/Resume/Education.js
@@ -1,9 +1,50 @@
 import React from "react";
+import ServiceCard from "../Services/ServiceCard";
 
 const Education = () => {
   return (
     <div>
-      <h2>this is education</h2>
+      <div class="relative max-w-2xl mx-auto mt-16">
+        <div class="absolute top-0 h-full border-r-2 border-black left-3"></div>
+        <ul class="space-y-2">
+          <li>
+            <div class="timeline-card flex items-center mb-10">
+              {/* <span class="w-3 h-3 mr-8 translate-x-1/2 bg-gray-500 rounded-full"></span> */}
+              <div className="service-card ml-10">
+                skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk
+                skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk
+                skjfhaksdjfhk skjfhaksdjfhk <br />
+                ksdlfkjdslfkj <br />
+                kjsdfksjhfdkj
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="timeline-card flex items-center mb-10">
+              {/* <span class="w-3 h-3 mr-8 translate-x-1/2 bg-gray-500 rounded-full"></span> */}
+              <div className="service-card ml-10">
+                skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk
+                skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk
+                skjfhaksdjfhk skjfhaksdjfhk <br />
+                ksdlfkjdslfkj <br />
+                kjsdfksjhfdkj
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="timeline-card flex items-center mb-10">
+              {/* <span class="w-3 h-3 mr-8 translate-x-1/2 bg-gray-500 rounded-full"></span> */}
+              <div className="service-card ml-10">
+                skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk
+                skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk skjfhaksdjfhk
+                skjfhaksdjfhk skjfhaksdjfhk <br />
+                ksdlfkjdslfkj <br />
+                kjsdfksjhfdkj
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/pages/Resume/SkillCard.js
+++ src/pages/Resume/SkillCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const SkillCard = ({ skill }) => {
   const { color, icon, name } = skill;
   return (
-    <p className="flex items-center">
+    <p className="flex items-center relative bottom-0 hover:bottom-2 transition-all duration-300">
       <span className={`${color} mr-2`}>{icon}</span> <span>{name}</span>
     </p>
   );
